57df24ed19c94b2cca6ff693028ed54477d08436
shashank-signalling <shashank.srivastava90@gmail.com>
2026-03-29 21:30:37 +0000

    Deploying to gh-pages from @ shashank-signalling/shashankthebetacateninguy.github.io@227d3ef6a94c7ee87682c01ef12dbd8c256032ac 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "shashankthebetacateninguy/shashankthebetacateninguy.github.io",
+    "data-repo": "shashank-signalling/shashankthebetacateninguy.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
